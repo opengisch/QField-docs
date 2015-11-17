81849979834884a60bdc325648e7fabcb7d3c225
Matthias Kuhn <matthias@opengis.ch>
2015-11-17 14:12:26 +0100

    Add deploy keAdd deploy key

--- scripts/ci/setup_git.sh
+++ scripts/ci/setup_git.sh
@@ -2,7 +2,7 @@
 
 if test "$TRAVIS_SECURE_ENV_VARS" = "true" -a "$TRAVIS_BRANCH" = "master";
 then
-  openssl aes-256-cbc -K $encrypted_cd22de430b6c_key -iv $encrypted_cd22de430b6c_iv -in opengisch_rsa.enc -out ~/.ssh/id_rsa -d;
+  openssl aes-256-cbc -K $encrypted_4f8d155c2793_key -iv $encrypted_4f8d155c2793_iv -in opengisch_rsa.enc -out ~/.ssh/id_rsa -d;
   chmod 600 ~/.ssh/id_rsa;
   git config --global user.email "info@opengis.ch";
   git config --global user.name "opengisch";
