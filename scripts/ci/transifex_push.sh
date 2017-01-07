c7c9835edff588030c7c0ed87df4e3e5ee44e2b1
Matthias Kuhn <matthias@opengis.ch>
2017-01-07 15:17:45 +0100

    Fix transifex autosync

--- scripts/ci/transifex_push.sh
+++ scripts/ci/transifex_push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if test "$TRAVIS_SECURE_ENV_VARS" = "true" -a "$TRAVIS_BRANCH" = "master";
 then
-  printf "[https://www.transifex.com]\nusername = opengisch\npassword = $TRANSIFEX_PASSWORD\n" > ~/.transifexrc
+  printf "[https://www.transifex.com]\nhostname = https://www.transifex.com\nusername = opengisch\npassword = $TRANSIFEX_PASSWORD\ntoken =\n" > ~/.transifexrc
   ./scripts/create_transifex_resources.sh;
   tx push -s
 fi
