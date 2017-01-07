0ed811df99df9babde6feb3625cda119e91a655b
Matthias Kuhn <matthias@opengis.ch>
2017-01-07 15:46:05 +0100

    Proper escaping in encryption and some cleanup

--- scripts/ci/transifex_push.sh
+++ scripts/ci/transifex_push.sh
@@ -2,6 +2,6 @@
 if test "$TRAVIS_SECURE_ENV_VARS" = "true" -a "$TRAVIS_BRANCH" = "master";
 then
   echo -e "[https://www.transifex.com]\nhostname = https://www.transifex.com\nusername = opengisch\npassword = $TRANSIFEX_PASSWORD\ntoken =\n" > ~/.transifexrc
-  ./scripts/create_transifex_resources.sh;
+  make gettext
   tx push -s
 fi
