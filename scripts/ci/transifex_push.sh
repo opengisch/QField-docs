7fe37b4154d99125437df69d5524117439148a52
Matthias Kuhn <matthias@opengis.ch>
2017-01-07 16:53:52 +0100

    Updates happen on master branch

--- scripts/ci/transifex_push.sh
+++ scripts/ci/transifex_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if test "$TRAVIS_SECURE_ENV_VARS" = "true" -a "$TRAVIS_BRANCH" = "tt";
+if test "$TRAVIS_SECURE_ENV_VARS" = "true" -a "$TRAVIS_BRANCH" = "master";
 then
   echo -e "[https://www.transifex.com]\nhostname = https://www.transifex.com\nusername = opengisch\npassword = $TRANSIFEX_PASSWORD\ntoken =\n" > ~/.transifexrc
   make gettext
