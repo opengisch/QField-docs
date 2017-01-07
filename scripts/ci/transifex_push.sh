41ed198fce0fd26f7f1254765db2af76e73d587c
Matthias Kuhn <matthias@opengis.ch>
2017-01-07 15:14:08 +0100

    Improve transifex autosync init

--- scripts/ci/transifex_push.sh
+++ scripts/ci/transifex_push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if test "$TRAVIS_SECURE_ENV_VARS" = "true" -a "$TRAVIS_BRANCH" = "master";
 then
+  printf "[https://www.transifex.com]\nusername = opengisch\npassword = $TRANSIFEX_PASSWORD\n" > ~/.transifexrc
   ./scripts/create_transifex_resources.sh;
-  tx init --user=opengisch --pass=$TRANSIFEX_PASSWORD
   tx push -s
 fi
