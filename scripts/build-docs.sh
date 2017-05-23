4b0e0485b0568a6f1df9db071f54e7ac1ee0d363
Matthias Kuhn <matthias@opengis.ch>
2017-05-23 08:07:12 +0200

    Only pull transifex when credentials are available

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -6,7 +6,12 @@ set -e
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/..
 pushd ${DIR}
 make clean
-make transifex_pull
+if test "$TRAVIS_SECURE_ENV_VARS" = "true" -a "$TRAVIS_BRANCH" = "master";
+then
+    make transifex_pull
+else
+    echo "Skipping translation because we are not on a secure master branch and therefore do not provide authentication for transifex."
+fi
 make html
 # make all-pdf # latex doesn't like utf-8
 popd
