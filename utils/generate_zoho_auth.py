d870235b1ce85efce0bcc8085c93770ebf6b24e7
Marco Bernasocchi <marco@opengis.ch>
2022-12-01 02:44:52 +0100

    add tools to import docs to KB articles

---
+++ utils/generate_zoho_auth.py
@@ -0,0 +1,17 @@
+# get from https://api-console.zoho.eu/
+CONFIG_ID = ""
+CONFIG_SECRET = ""
+CONFIG_CODE = ""
+
+if not CONFIG_CODE:
+    exit("Visit https://api-console.zoho.eu/ and generate a CONFIG_CODE for a self client with scope `Desk.articles.CREATE, Desk.articles.UPDATE, Desk.tickets.ALL`")
+
+AUTH_URL = "https://accounts.zoho.eu/oauth/v2/token?code={}&grant_type=authorization_code&client_id={}&client_secret={}&redirect_uri=https://www.qfield.cloud".format(CONFIG_CODE, CONFIG_ID, CONFIG_SECRET)
+auth  = requests.post(AUTH_URL)
+try:
+    print(auth.json()["access_token"])
+    print(auth.json()["refresh_token"])
+    
+except KeyError:
+    exit(auth.text)
+
---
+++ utils/push_to_zoho.py
@@ -0,0 +1,99 @@
+import yaml
+import markdown
+import re
+import requests
+
+import os.path
+import materialx.emoji
+
+from collections.abc import Iterable
+
+
+AUTH_CODE = 'q'
+if not AUTH_CODE:
+    exit("ERROR: use generate_zoho_auth.py to generate the oauth2 code")
+
+
+# skip !ENV tags see https://github.com/yaml/pyyaml/issues/86
+yaml.add_multi_constructor('!ENV', lambda loader, suffix, node: None)
+
+
+def extract_path(paths):
+    flat_paths = []
+    if isinstance(paths, str):
+        return paths
+    elif isinstance(paths, dict):
+        for _, path in paths.items():
+            flat_paths.append(extract_path(path))
+
+    elif isinstance(paths, list):
+        for path in paths: 
+            flat_paths.append(extract_path(path))
+    return flat_paths
+
+
+def flatten(xs):
+    for x in xs:
+        if isinstance(x, Iterable) and not isinstance(x, (str, bytes)):
+            yield from flatten(x)
+        else:
+            yield x
+
+
+def convert_md(path):
+    with open(path, 'r') as file:
+        md_text = file.read()
+
+    docs_url = "https://docs.qfield.org"
+    docs_url = path.replace('documentation', docs_url)[:-6]
+    docs_url_warning = "> ***The complete documentation is at <{}>***\n\n".format(docs_url)
+
+    header, body = md_text.split('#', 1)
+    body = "{}#{}".format(docs_url_warning, body)
+    
+    html = markdown.markdown(body)
+    title = re.search('title: (.*)\n', header).group(1)
+    slug = re.search('tx_slug: (.*)\n', header).group(1)
+    return(slug, title, html)
+
+
+def publish_article(category, path):
+    slug, title, html = convert_md(path)
+
+    url = 'https://desk.zoho.eu/api/v1/articles?'
+
+    payload = {
+        "permission" : "ALL",
+        "title" : title,
+        "permalink" : slug,
+        "answer" : html,
+        "categoryId" : 116946000000449024,
+        "status" : "Published"
+        }
+    headers = {
+        'Authorization': "Zoho-oauthtoken "+ AUTH_CODE,
+    }
+
+    req = requests.post(url, json = payload, headers=headers)
+
+####################################
+#######Main#########################
+####################################
+
+with open('mkdocs.yml', 'r') as file:
+    config = yaml.full_load(file)
+articles = config['nav']
+
+for category in articles: 
+    for category_title, paths in category.items():
+        paths = extract_path(paths)
+        if isinstance(paths, str):
+            paths = [paths]
+        else:
+            paths = list(flatten(paths))
+        
+        for path in paths:
+            if path == "index.md":
+                continue
+            path =  "documentation/{}.en.md".format(path[:-3])
+            publish_article(category_title, path)
