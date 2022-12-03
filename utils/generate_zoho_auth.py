f331ae1ff9d624ea1cd63b786c826d45cdc23796
Marco Bernasocchi <marco@opengis.ch>
2022-12-03 21:11:49 +0100

    allow refreshing token

--- utils/generate_zoho_auth.py
+++ utils/generate_zoho_auth.py
@@ -5,11 +5,15 @@ CONFIG_ID = ""
 CONFIG_SECRET = ""
 CONFIG_CODE = ""
 
-if not CONFIG_CODE:
-    exit("Visit https://api-console.zoho.eu/ and generate a CONFIG_CODE for a self client with scope `Desk.articles.CREATE, Desk.articles.UPDATE, Desk.tickets.ALL`")
+REFRESH_TOKEN = ""
 
-AUTH_URL = "https://accounts.zoho.eu/oauth/v2/token?code={}&grant_type=authorization_code&client_id={}&client_secret={}&redirect_uri=https://www.qfield.cloud".format(CONFIG_CODE, CONFIG_ID, CONFIG_SECRET)
-auth  = requests.post(AUTH_URL)
+if not CONFIG_CODE:
+    exit("Visit https://api-console.zoho.eu/ and generate a CONFIG_CODE for a self client with scope `Desk.articles.CREATE, Desk.articles.UPDATE, Desk.articles.READ`")
+
+BASE_URL = "https://accounts.zoho.eu/oauth/v2/token"
+AUTH_URL = "{}?code={}&grant_type=authorization_code&client_id={}&client_secret={}&redirect_uri=https://www.qfield.cloud".format(BASE_URL, CONFIG_CODE, CONFIG_ID, CONFIG_SECRET)
+REFRESH_URL = "{}?refresh_token={}&client_id={}&client_secret={}&scope=Desk.articles.CREATE,Desk.articles.UPDATE,Desk.articles.READ&grant_type=refresh_token".format(BASE_URL, REFRESH_TOKEN, CONFIG_ID, CONFIG_SECRET)
+auth  = requests.post(REFRESH_URL)
 try:
     print('access_token: ', auth.json()["access_token"])
     print('refresh_token: ', auth.json()["refresh_token"])
