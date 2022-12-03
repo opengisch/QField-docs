ddfe7b00ed0395c0e537958cf03d3eec36d8bbc4
Marco Bernasocchi <marco@opengis.ch>
2022-12-03 11:26:40 +0100

    cleanup auth code

--- utils/generate_zoho_auth.py
+++ utils/generate_zoho_auth.py
@@ -1,3 +1,5 @@
+import requests
+
 # get from https://api-console.zoho.eu/
 CONFIG_ID = ""
 CONFIG_SECRET = ""
@@ -9,8 +11,8 @@ if not CONFIG_CODE:
 AUTH_URL = "https://accounts.zoho.eu/oauth/v2/token?code={}&grant_type=authorization_code&client_id={}&client_secret={}&redirect_uri=https://www.qfield.cloud".format(CONFIG_CODE, CONFIG_ID, CONFIG_SECRET)
 auth  = requests.post(AUTH_URL)
 try:
-    print(auth.json()["access_token"])
-    print(auth.json()["refresh_token"])
+    print('access_token: ', auth.json()["access_token"])
+    print('refresh_token: ', auth.json()["refresh_token"])
     
 except KeyError:
     exit(auth.text)
