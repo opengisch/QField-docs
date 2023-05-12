bdb400b92786b9f7b03e79a6b9e681190ce672e4
Marco Bernasocchi <marco@opengis.ch>
2023-05-12 15:01:18 +0200

    refactor auth code

--- utils/generate_zoho_auth.py
+++ utils/generate_zoho_auth.py
@@ -1,22 +1,51 @@
 import requests
 import os
 
-# get from https://api-console.zoho.eu/ or in GH secrets
+
 CONFIG_ID = os.environ['ZOHO_CONFIG_ID']
 CONFIG_SECRET = os.environ['ZOHO_CONFIG_SECRET']
 CONFIG_CODE = os.environ['ZOHO_CONFIG_CODE']
 REFRESH_TOKEN = os.environ['ZOHO_REFRESH_TOKEN']
 
-if not CONFIG_CODE:
-    exit("Visit https://api-console.zoho.eu/ and generate a CONFIG_CODE for a self client with scope `Desk.articles.CREATE, Desk.articles.UPDATE, Desk.articles.READ`")
-
 BASE_URL = "https://accounts.zoho.eu/oauth/v2/token"
-AUTH_URL = "{}?code={}&grant_type=authorization_code&client_id={}&client_secret={}&redirect_uri=https://www.qfield.cloud".format(BASE_URL, CONFIG_CODE, CONFIG_ID, CONFIG_SECRET)
-REFRESH_URL = "{}?refresh_token={}&client_id={}&client_secret={}&scope=Desk.articles.CREATE,Desk.articles.UPDATE,Desk.articles.READ&grant_type=refresh_token".format(BASE_URL, REFRESH_TOKEN, CONFIG_ID, CONFIG_SECRET)
-auth  = requests.post(AUTH_URL)
-try:
-    print('access_token: ', auth.json()["access_token"])
-    print('refresh_token: ', auth.json()["refresh_token"])
 
-except KeyError:
-    exit(auth.text)
+
+def create_access_secrets():
+    """
+    Used to generate CONFIG_CODE and REFRESH_TOKEN
+    requires manual interaction.
+    Before running it you need to create a valid auth on 
+    Visit https://api-console.zoho.eu/ and generate a new CONFIG_CODE 
+    for a self client with scope 
+    `Desk.articles.CREATE, Desk.articles.UPDATE, Desk.articles.READ`
+    """
+    
+    url = "{}?code={}&grant_type=authorization_code&client_id={}&client_secret={}&redirect_uri=https://www.qfield.cloud".format(BASE_URL, CONFIG_CODE, CONFIG_ID, CONFIG_SECRET)
+    auth  = requests.post(url)
+    try:
+        access_token = auth.json()["access_token"]
+        refresh_token = auth.json()["refresh_token"]
+        tokens = {'access_token': access_token,
+                'refresh_token': refresh_token}
+        return tokens
+
+    except KeyError:
+        print(auth.text) 
+        print("Visit https://api-console.zoho.eu/ and generate a new CONFIG_CODE for a self client with scope `Desk.articles.CREATE, Desk.articles.UPDATE, Desk.articles.READ`")
+
+
+def get_fresh_token():
+    """used to get a new authentication code without manual interaction"""
+    
+    url = "{}?refresh_token={}&client_id={}&client_secret={}&scope=Desk.articles.CREATE,Desk.articles.UPDATE,Desk.articles.READ&grant_type=refresh_token".format(BASE_URL, REFRESH_TOKEN, CONFIG_ID, CONFIG_SECRET)
+    auth  = requests.post(url)
+    
+    try:
+        token = auth.json()["access_token"]
+        return token
+
+    except KeyError:
+        print(auth.text) 
+    
+
+# print(get_fresh_token())
